eff96caeb26a7596f05d8e3caafb4853935b5b9f
RaunakSMatharu <matharu.r@northeastern.edu>
2022-10-27 16:46:42 -0400

    First Commit

---
+++ src/model/Doctor.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author Raunak Singh Matharu
+ */
+public class Doctor {
+    private String Doctor_Name;
+    private int Doctor_ID;
+    private int Doctor_age;
+    private String Doctor_Gender;
+    private String Doctor_City;
+    private String Hospital_Assoicated;
+    private String Doctor_Speacilization;
+    private String Doctor_BloodGroup;
+    private String Doctor_Cell_No;
+    private String Doctor_Email_ID; 
+
+    public String getDoctor_Name() {
+        return Doctor_Name;
+    }
+
+    public void setDoctor_Name(String Doctor_Name) {
+        this.Doctor_Name = Doctor_Name;
+    }
+
+    public int getDoctor_ID() {
+        return Doctor_ID;
+    }
+
+    public void setDoctor_ID(int Doctor_ID) {
+        this.Doctor_ID = Doctor_ID;
+    }
+
+    public int getDoctor_age() {
+        return Doctor_age;
+    }
+
+    public void setDoctor_age(int Doctor_age) {
+        this.Doctor_age = Doctor_age;
+    }
+
+    public String getDoctor_Gender() {
+        return Doctor_Gender;
+    }
+
+    public void setDoctor_Gender(String Doctor_Gender) {
+        this.Doctor_Gender = Doctor_Gender;
+    }
+
+    public String getDoctor_City() {
+        return Doctor_City;
+    }
+
+    public void setDoctor_City(String Doctor_City) {
+        this.Doctor_City = Doctor_City;
+    }
+
+    public String getHospital_Assoicated() {
+        return Hospital_Assoicated;
+    }
+
+    public void setHospital_Assoicated(String Hospital_Assoicated) {
+        this.Hospital_Assoicated = Hospital_Assoicated;
+    }
+
+    public String getDoctor_Speacilization() {
+        return Doctor_Speacilization;
+    }
+
+    public void setDoctor_Speacilization(String Doctor_Speacilization) {
+        this.Doctor_Speacilization = Doctor_Speacilization;
+    }
+
+    public String getDoctor_BloodGroup() {
+        return Doctor_BloodGroup;
+    }
+
+    public void setDoctor_BloodGroup(String Doctor_BloodGroup) {
+        this.Doctor_BloodGroup = Doctor_BloodGroup;
+    }
+
+    public String getDoctor_Cell_No() {
+        return Doctor_Cell_No;
+    }
+
+    public void setDoctor_Cell_No(String Doctor_Cell_No) {
+        this.Doctor_Cell_No = Doctor_Cell_No;
+    }
+
+    public String getDoctor_Email_ID() {
+        return Doctor_Email_ID;
+    }
+
+    public void setDoctor_Email_ID(String Doctor_Email_ID) {
+        this.Doctor_Email_ID = Doctor_Email_ID;
+    }
+      
+}
---
+++ src/model/Hospital.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Raunak Singh Matharu
+ */
+public class Hospital {
+    private String Hospital_Name;
+    private String Hospital_City;
+    private int Hospital_pincode;
+    public String getHospital_Name() {
+        return Hospital_Name;
+    }
+
+    public void setHospital_Name(String Hospital_Name) {
+        this.Hospital_Name = Hospital_Name;
+    }
+
+    public String getHospital_City() {
+        return Hospital_City;
+    }
+
+    public void setHospital_City(String Hospital_City) {
+        this.Hospital_City = Hospital_City;
+    }
+
+    public int getHospital_pincode() {
+        return Hospital_pincode;
+    }
+
+    public void setHospital_pincode(int Hospital_pincode) {
+        this.Hospital_pincode = Hospital_pincode;
+    }
+    
+}
